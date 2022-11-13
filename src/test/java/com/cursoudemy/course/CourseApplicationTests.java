9ba59d0f1cbb8717035d178f6bf34be390330e8a
Bruno Pinheiro da Silva <bruno.pinheiro@ituran.com.br>
2022-11-13 19:20:32 -0300

    first commit

---
+++ src/test/java/com/cursoudemy/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoudemy.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
