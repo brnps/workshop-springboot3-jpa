f00e7ea5aeda798905eca89ef7639d2db8019167
Bruno Pinheiro da Silva <bruno.pinheiro@ituran.com.br>
2022-11-13 21:51:48 -0300

    Category

--- src/main/java/com/cursoudemy/course/config/TestConfig.java
+++ src/main/java/com/cursoudemy/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.cursoudemy.course.config;
 
+import com.cursoudemy.course.entities.Category;
 import com.cursoudemy.course.entities.Order;
 import com.cursoudemy.course.entities.User;
 import com.cursoudemy.course.entities.enums.OrdersStatus;
+import com.cursoudemy.course.repositories.CategoryRepository;
 import com.cursoudemy.course.repositories.OrderRepository;
 import com.cursoudemy.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +22,18 @@ public class TestConfig implements CommandLineRunner {
     private UserRepository userRepository;
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -33,5 +43,6 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
